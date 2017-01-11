74d9fcd1bcc30894bbab6fc3265be76f797a8ddc
Lee Shiueh <lee.shiueh@gmail.com>
2017-01-11 15:28:33 +0800

    pikeeper.server module draft release

--- pikeeper/models.py
+++ pikeeper/models.py
@@ -13,3 +13,14 @@ class Report(models.Model):
     services = models.CharField('service list', max_length=100,default='')
     local_ip_list = models.CharField('local ip list', max_length=100,default='')
     public_key = models.CharField('public key', max_length=500,default='')
+    
+    def __unicode__(self):
+        return str((self.version,
+                    self.hardware,
+                    self.revision,
+                    self.serial,
+                    self.public_ip,
+                    str(self.local_ip_list),
+                    str(self.last_update_time),
+                    str(self.services),
+                    self.public_key[:15]+'...'+self.public_key[-30:]))
--- pikeeper/server.py
+++ pikeeper/server.py
@@ -6,6 +6,10 @@ from django.http import HttpResponse, Http404
 import logging
 logger = logging.getLogger(__name__)
 
+from ipware.ip import get_real_ip
+import json
+from datetime import datetime
+
 from .models import Report
 
 class ApiReport_v1(View):
@@ -15,9 +19,27 @@ class ApiReport_v1(View):
             encrypted_data = request.POST.get('data',None)
             logger.debug('encrypted_data digest: %s' % encrypted_data[:20])
             
-            client_secret_key = settings.PIKEEPER['client_secret_key']
-            client_report = signing.loads(encrypted_data,client_secret_key)
-            logger.debug('client report keys: %s' % str(client_report.keys()))
+            secret_key = settings.PIKEEPER['client_secret_key']
+            report = signing.loads(encrypted_data,secret_key)
+            logger.debug('client report keys: %s' % str(report.keys()))
+            rpi_serial = report.get('cpu_info',{}).get('serial',None)
+            if not rpi_serial is None:
+                db_entry,_ = Report.objects.get_or_create(serial=rpi_serial)
+                db_entry.serial = rpi_serial
+                db_entry.hardware = report.get('cpu_info',{}).get('hardware','')
+                db_entry.revision = report.get('cpu_info',{}).get('revision','')
+                public_ip = get_real_ip(request)
+                if public_ip is None:
+                    public_ip = ''
+                db_entry.public_ip = public_ip
+                db_entry.services = str(report.get('services',[]))
+                db_entry.local_ip_list = str(report.get('local_ip',[]))
+                db_entry.public_key = str(report.get('public_key',''))
+                db_entry.last_update_time = datetime.now()
+                db_entry.save()
+                logger.info('ApiReport_v1: %s' % str(db_entry) )
+            else:
+                logger.warning('ApiReport_v1 no serial: %s' % json.dumps(report))
             
             return HttpResponse('OK')
         except:
